d8f6161296eea0a08c08602a4e3b9241ee47d2c3
nhh2 <nhernand@chicagobooth.edu>
2023-02-23 03:00:01 +0000

    Follow the Frigate_v3 Copying Snowing Pine substance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,20 @@ class ApplicationController < ActionController::Base
     render ({ :template => "home_template/main_page.html.erb"})
   end
 
+  def writing_index
+    render ({ :template => "writing_template/index.html.erb"})
+  end
+
+  def about_index
+    render ({ :template => "about_template/index.html.erb"})
+  end
+
+  def books_index
+    render ({ :template => "books_template/index.html.erb"})
+  end
+
+  def now_index
+    render ({ :template => "now_template/index.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
 
-  get("/",{ :controller => "application", :action => "index" })
+  # Homepage
+    get("/",{ :controller => "application", :action => "writing_index" })
+
+  # Writing
+    get("/writing",{ :controller => "application", :action => "writing_index" })
+
+  # About
+    get("/about",{ :controller => "application", :action => "about_index" })
+
+  # Books
+    get("/books",{ :controller => "application", :action => "books_index" })
+
+  # Now
+    get("/now",{ :controller => "application", :action => "now_index" })
 
 end
