bb3589e260414b03ce7d30c720dea760534df0da
nhh2 <nhernand@chicagobooth.edu>
2023-03-04 22:39:08 +0000

    v8 -- Table working correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
 
   def writing_index
+    matching_posts = Post.all
+    @list_of_posts = matching_posts.order({ :created_at => :asc })
+
     render ({ :template => "writing_template/index.html.erb"})
   end
 
