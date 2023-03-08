3190b4ff310037e87f9d84f91291324c2ee23c55
nhh2 <nhernand@chicagobooth.edu>
2023-03-08 05:34:04 +0000

    Publish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  Post.destroy_all
+
+  Post.create!([
+    {title: "Test1"},
+    {title: "Test2"},
+    {title: "Test3"},
+    {title: "Test4"}
+      ])
+
+  p "Created #{Post.count} posts"
